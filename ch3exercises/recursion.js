5964660e6c66488827bb9c52e3959e230480a146
Kat Wooters <c41t@live.com>
2022-07-12 13:50:18 -0500

    updated recursion exercise

--- ch3exercises/recursion.js
+++ ch3exercises/recursion.js
@@ -1,3 +1,9 @@
+let text1 = "The number ";
+let text2 = " is even: "
+let number = 0;
+let answerText = text1.concat(number, text2);
+
+
 function isEven(n) {
     if (n == 0) {
         return true;
@@ -10,7 +16,38 @@ function isEven(n) {
     };
 };
 
-//Tests:
-  console.log(isEven(10));
-  console.log(isEven(47));
-  console.log(isEven(-12));
+function updateText(value) {
+    if (isEven(value) === false){
+        text2 = " is odd."
+    } else {
+        text2 = " is even."
+    }
+    answerText = text1.concat(value, text2);
+    console.log(answerText);
+}
+
+updateText(10);
+updateText(47);
+updateText(-12);
+
+/*Tests:
+number = 10;
+   answerText = text1.concat(number, text2);
+  isEven(number);
+  console.log(answerText);
+answerText = text1.concat(number, text2);
+console.log(answerText + isEven(number));
+
+number = 47;
+    answerText = text1.concat(number, text2);
+    isEven(number);
+    console.log(answerText);
+answerText = text1.concat(number, text2);
+ /  console.log(answerText + isEven(number));
+
+number = -12;
+    answerText = text1.concat(number, text2);
+    isEven(number);
+    console.log(answerText);
+answerText = text1.concat(number, text2);
+    console.log(answerText + isEven(number)); */
